123dc6c812ec08b430dbc321ae03aa2987e7d09d
puzzleCatcher <74927928+puzzleCatcher@users.noreply.github.com>
2023-08-31 17:57:40 -0700

    Update designLab01Work.py
    finished fib/v2, started polynomial

--- designLab01/designLab01Work.py
+++ designLab01/designLab01Work.py
@@ -8,18 +8,56 @@
 #-----------------------------------------------------------------------------
 
 def fib(n):
-    # Delete the pass statement below and insert your own code
-    pass
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-2) + fib(n-1)
 
 #-----------------------------------------------------------------------------
 
 class V2:
-    # Delete the pass statement below and insert your own code
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+    def __str__(self):
+        return f"V2[{self.x}, {self.y}]"
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def add(self, v):
+        return self.__add__(v)
+
+    def __add__(self, v):
+        return V2(self.x + v.x, self.y + v.y)
+
+    def mul(self, s):
+        return self.__mul__(s)
+
+    def __mul__ (self, s):
+        return V2(self.x * s, self.y * s)
+
+a = V2(1.0, 2.0)
+b = V2(2.2, 3.3)
+print(a.add(b))
+
+print(a.mul(2))
+
+print(a.add(b).mul(-1))
+
+print(V2(1.1, 2.2) + V2(3.3, 4.4))
 #-----------------------------------------------------------------------------
 
 class Polynomial:
-    # Delete the pass statement below and insert your own code
-    pass
-
+    def __init__(self, c):
+        self.c = c
+    
+    
+    
+    # def __add__(self, p):
+        
